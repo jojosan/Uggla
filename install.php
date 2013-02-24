06eb4b1531d53b7810e39eece66af1377ac1ed27
Dennis Mader <mader.dennis@live.de>
2013-02-24 15:59:11 +0100

    Installationsprozess Schritt 1/3

--- install.php
+++ install.php
@@ -3,18 +3,15 @@
 include_once('includes/connection.php');
 
 /* Erstelle Datenbank Tabelle beim Aufrufen der Seite*/
-	$db = $pdo->prepare('CREATE TABLE IF NOT EXISTS db ( 
-	  user_id int(11) NOT NULL AUTO_INCREMENT,
-	  user_name varchar(20) NOT NULL,
-	  user_password varchar(255) NOT NULL,
-	  user_email varchar(40) NOT NULL,
-	  user_group varchar(40) NOT NULL,
-	  PRIMARY KEY (user_id)
+	$db = $pdo->prepare('CREATE TABLE IF NOT EXISTS config ( 
+	  webseiten_name varchar(100) NOT NULL,
+	  theme varchar(255) NOT NULL,
+	  admin_name varchar(60) NOT NULL,
+	  admin_mail varchar(100) NOT NULL,
+	  admin_password varchar(255) NOT NULL
 	)');
-	$db->execute();
-	
-/*  Wenn SUBMIT dann erstelle users Tabelle*/
-if (isset($_POST['Submit'])) {
+	$db->execute();	
+
 	$users = $pdo->prepare('CREATE TABLE IF NOT EXISTS users (
 	  user_id int(11) NOT NULL AUTO_INCREMENT,
 	  user_name varchar(20) NOT NULL,
@@ -26,10 +23,30 @@ if (isset($_POST['Submit'])) {
 	$users->execute();
 	if (mysql_errno() == 0) {
 		$error = 'Benutzer-Tabelle erfolgreich erstellt!';	
+		/* header('Location: install_2.php');*/
 	}
 	else{ // Wenn MySQL Fehler..
 		$error = 'Fehler bei der Benutzer-Tabelle ". mysql_errno() .":". mysql_error()."';	
 	}
+/*  Wenn SUBMIT dann erstelle users Tabelle*/
+if (isset($_POST['Submit'])) {
+	//Daten aus Form in Tabelle config eintragen
+		$webseiten_name = $_POST['webseiten_name'];
+		$admin_name = $_POST['admin_name'];
+		$admin_mail = $_POST['admin_mail'];
+		$admin_password = @$_POST['admin_password'];
+		if (empty($webseiten_name) or empty($admin_name)) {
+			$error = 'Alle Felder ausfüllen';	
+		} else {
+	$query = $pdo->prepare('INSERT INTO config (webseiten_name, admin_name, admin_mail, admin_password) VALUES (?, ?, ?, ?)');
+			
+			$query->bindValue(1, $webseiten_name);
+			$query->bindValue(2, $admin_name);
+			$query->bindValue(3, $admin_mail);	
+			$query->bindValue(4, md5($admin_password));
+			
+			$query->execute();
+}
 }
 ?>
 <!doctype html>
@@ -54,10 +71,10 @@ if (isset($_POST['Submit'])) {
                 <br /><br />
                 <?php } ?>
                      <form id="install" action="install.php" method="post">       
-                        <input type="text" name="host" placeholder="localhost"/><br />
-                        <input type="text" name="dbname" placeholder="Datenbankname" /><br />
-                        <input type="text" name="dbuser" placeholder="Datenbank User" /><br />
-                        <input type="text" name="dbpassswort" placeholder="Datenbank Passwort" /><br />
+                        <input type="text" name="webseiten_name" placeholder="Name deiner Webseite"/><br />
+                        <input type="text" name="admin_name" placeholder="Name des Admin Accounts" /><br />
+                        <input type="text" name="admin_mail" placeholder="Deine E-Mail Adresse" /><br />
+                        <input type="password" name="admin_passsword" placeholder="Dein Passwort" /><br />
                         <input type="submit" name="Submit" id="Submit" value="Speichern" />
                     </form>	
              </div>
