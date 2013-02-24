b7a966629d297367c57efe827037cad5dcf5f1d1
Dennis Mader <mader.dennis@live.de>
2013-02-24 15:59:45 +0100

    Bug fixes

--- includes/connection.php
+++ includes/connection.php
@@ -1,9 +1,14 @@
-<?php	
+<?php
 	try {	
-$pdo = new PDO('mysql:host=localhost;dbname=cms', 'root', 'root');
+$pdo = new PDO('mysql:
+host=localhost; 
+dbname=cms', /* Der Name deiner Datenbank */
+'root', /* Dein Benutzername der Datenbank */
+'root'); /* Dein Passwort für die Datenbank. Alle Daten findest du bei deinem Webspace Hoster. */
 } catch (PDOExeption $e) {
 	exit('Database error.');
 
 }
+
 
 ?>
