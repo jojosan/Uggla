e31c71bccf1befb9dfbe5e4869df7cbd0e0537e8
lukasepple <lukas.epple@me.com>
2013-03-24 18:24:54 +0100

    Bug fixes

--- template/home.php
+++ template/home.php
@@ -4,7 +4,7 @@
 			$posts = $articles->fetch_all("article_timestamp", "ASC");
 			foreach($posts as $article){
 				echo "<h2><a href='article.php?id=".$article->id."'>".$article->title."</a></h2>\n";
-				echo "<p>".substr($article->content, 0, 140)."[...]</p>";
+				echo "<p>".$article->content."[...]</p>";
 			}			
 			$template->get("footer.php");
 			?>
