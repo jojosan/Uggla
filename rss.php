273e0271120bf6cfc7e48da137cba3dd1d518215
lukasepple <lukas.epple@me.com>
2013-04-07 17:01:01 +0200

    RSS-Feed hinzugefügt Er zeigt die letzten zehn Einträge an.

---
+++ rss.php
@@ -0,0 +1,42 @@
+<?php
+	header("Contet-type: application/rss+xml");
+	include("./includes/config.php");
+	include("./includes/connection.php");
+	include("./includes/system.php");
+	$articles = new Articles();
+	$pageURL = 'http';
+	if ($_SERVER["HTTPS"] == "on") {$pageURL .= "s";}
+	$pageURL .= "://";
+	if ($_SERVER["SERVER_PORT"] != "80") {
+		$pageURL .= $_SERVER["SERVER_NAME"].":".$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"];
+	} else {
+	$pageURL .= $_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"];
+	}
+	$pageURL =  str_replace("rss.php", "", $pageURL);
+echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
+?>
+
+<rss version="2.0">
+	<channel>
+    		<title><?php echo SITE_NAME; ?></title>
+    		<link><?php echo $pageURL; ?></link>
+    		<description>My awesome site</description>
+    		<language>de-de</language>
+    		<!--<copyright></copyright>-->
+    		<pubDate><?php echo date("r"); ?></pubDate><?php
+			$c = 0;
+			foreach($articles->fetch_all("article_timestamp", "DESC") as $article){
+				if($c == 10){ break; } ?>
+
+		<item>
+			<title><?php echo $article->title; ?></title>
+			<description><?php echo $article->content; ?></description>
+			<link><?php echo $pageURL."article.php?id=".$article->id ?></link>
+			<author><?php echo $article->author ?></author>
+			<guid><?php echo $pageURL."article.php?id=".$article->id ?></guid>
+			<pubDate><?php echo date("r", $article->timestamp); ?></pubDate>
+		</item>
+		<?php $c++; } ?>
+	
+	</channel>
+</rss>
--- template/header.php
+++ template/header.php
@@ -4,6 +4,7 @@
 		<meta charset="utf-8"/>
 		<title><?php echo SITE_NAME;  ?></title>
 		<link rel="stylesheet" href="assets/style.css"/>
+		<link rel="alternate" type="application/rss+xml" title="RSS" href="./rss.php" />
 	</head>
 	<body>
 		<div class="container">
