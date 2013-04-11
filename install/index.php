9804ff648933b2e120332c91c8b859204fafed32
Weltraumakustik <hallo@weltraumakustik.de>
2013-04-11 20:51:21 +0200

    Neues Feld für Datenbank hinzugefügt: user_mail

--- install/index.php
+++ install/index.php
@@ -20,6 +20,7 @@ if(isset($_POST['db_server'], $_POST['db_name'], $_POST['db_user'], $_POST['db_p
   `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_name` varchar(255) NOT NULL,
   `user_password` varchar(255) NOT NULL,
+  `user_mail` varchar(255) NOT NULL,
   PRIMARY KEY (`user_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ");
 
