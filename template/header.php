4227f2a837127069a0d60396983bec23df93456d
Weltraumakustik <hallo@weltraumakustik.de>
2013-04-12 22:54:43 +0200

    Einbindung für TinyMCE

--- template/header.php
+++ template/header.php
@@ -5,6 +5,9 @@
 		<title><?php echo SITE_NAME;  ?></title>
 		<link rel="stylesheet" href="assets/style.css"/>
 		<link rel="alternate" type="application/rss+xml" title="RSS" href="./rss.php" />
+		
+		<!-- WICHTIG; SOLLTE SPÄTER WIE BEI WORDPRESS ÜDER EINE FUNCTION EINGEBUNDEN WERDEN -->
+		<script type="text/javascript" src="includes/lib/tiny_mce/tiny_mce.js"></script>
 	</head>
 	<body>
 		<div class="container">
