8248d6965c802e16ff9b3dcb086f58f60af5d5e1
Weltraumakustik <hallo@weltraumakustik.de>
2013-04-14 00:42:51 +0200

    Admin_Template (+)

--- includes/config.php
+++ includes/config.php
@@ -1,12 +1,10 @@
 <?php
-
-/* DB */
-define('DB_SERVER', 'localhost');
-define('DB_NAME', 'weltraum');
-define('DB_USER', 'root');
-define('DB_PASS', 'toor');
-
-/* Site Info */
-define('SITE_NAME', 'Lukas Pina');
-
-?>
+ /*DB*/
+ define('DB_SERVER', 'localhost');
+  define('DB_NAME', 'pina');
+ define('DB_USER', 'root');
+ define('DB_PASS', 'root');
+ /*Site Info*/
+ define('SITE_NAME', 'Weltraum');
+ define('ADMIN_TEMPLATE', 'flat');
+ ?>
