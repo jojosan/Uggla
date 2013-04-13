6dba90868d7b3fabef5b59784333b65d3b6a5c6d
Weltraumakustik <hallo@weltraumakustik.de>
2013-04-14 00:56:45 +0200

    Fix

--- includes/config.php
+++ includes/config.php
@@ -1,10 +1,11 @@
 <?php
  /*DB*/
  define('DB_SERVER', 'localhost');
-  define('DB_NAME', 'pina');
+ define('DB_NAME', 'pina');
  define('DB_USER', 'root');
  define('DB_PASS', 'root');
  /*Site Info*/
  define('SITE_NAME', 'Weltraum');
+ /*Site STYLE*/
  define('ADMIN_TEMPLATE', 'flat');
  ?>
