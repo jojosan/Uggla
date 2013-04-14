52f0d3717bb52080b12b629ca0367debe6abfe96
lukasepple <lukas.epple@me.com>
2013-04-14 11:53:04 +0200

    Bugfix

--- includes/connection.php
+++ includes/connection.php
@@ -2,8 +2,7 @@
 
 try
 {
-    $pdo = new PDO('mysql:
-	host='.DB_SERVER.';dbname='.DB_NAME, DB_USER, DB_PASS); // Siehe config.php Alle Daten findest du bei deinem Webspace Hoster.
+    $pdo = new PDO('mysql:host='.DB_SERVER.';dbname='.DB_NAME, DB_USER, DB_PASS); // Siehe config.php Alle Daten findest du bei deinem Webspace Hoster.
 }
 catch (PDOExeption $e)
 {
